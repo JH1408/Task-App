44160cca230c9e5544f04ac7aa07f39dc97018b5
Josy Hartig <josy_hartig@yahoo.de>
2019-07-24 22:21:28 +0800

    add patch and delete routes

--- src/index.js
+++ src/index.js
@@ -63,34 +63,117 @@ app.get('/users/:id', async (req, res) => {
   // });
 });
 
-// Task routes
-app.post('/tasks', (req, res) => {
-  const task = new Task(req.body);
-  task.save().then(() => {
-    res.status(201).send(task);
-  }).catch((err) => {
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+  if(!isValidOperation) {
+    return res.status(400).send({error: 'Invalid updates!'});
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (err) {
     res.status(400).send(err);
-  });
+  }
 });
 
-app.get('/tasks', (req, res) => {
-  Task.find({}).then((tasks) => {
-      res.send(tasks);
-  }).catch((err) => {
-      res.status(500).send();
-  });
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if(!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch(err) {
+    res.status(500).send();
+  }
 });
 
-app.get('/tasks/:id', (req, res) => {
+// Task routes
+app.post('/tasks', async (req, res) => {
+  const task = new Task(req.body);
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+  // task.save().then(() => {
+  //   res.status(201).send(task);
+  // }).catch((err) => {
+  //   res.status(400).send(err);
+  // });
+});
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch(err) {
+    res.status(500).send();
+  }
+//   Task.find({}).then((tasks) => {
+//       res.send(tasks);
+//   }).catch((err) => {
+//       res.status(500).send();
+//   });
+ });
+
+app.get('/tasks/:id', async (req, res) => {
   const id = req.params.id;
-  Task.findById(id).then((task) => {
-      if(!task) {
-        return res.status(404).send();
-      }
-      res.send(task);
-  }).catch((err) => {
-      res.status(500).send();
-  });
+  try {
+    const task = await Task.findById(id);
+    if(!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch(err) {
+    res.status(500).send();
+  }
+  // Task.findById(id).then((task) => {
+  //     if(!task) {
+  //       return res.status(404).send();
+  //     }
+  //     res.send(task);
+  // }).catch((err) => {
+  //     res.status(500).send();
+  // });
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['description', 'completed'];
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+  if(!isValidOperation) {
+    return res.status(400).send({error: 'Invalid updates!'});
+  }
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if(!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch(err) {
+    res.status(500).send();
+  }
 });
 
 
