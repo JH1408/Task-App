5de58dae3e4320969d0eeb11ab6290c54b780c8e
Josy Hartig <josy_hartig@yahoo.de>
2019-07-26 16:17:49 +0800

    add user task relationship

--- src/models/user.js
+++ src/models/user.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -50,6 +51,12 @@ const userSchema = new mongoose.Schema({
   }]
 });
 
+userSchema.virtual('tasks', {
+  ref: 'Task',
+  localField: '_id',
+  foreignField: 'author'
+});
+
 // Create new method for instance
 userSchema.methods.generateAuthToken = async function() {
   const user = this;
@@ -59,6 +66,14 @@ userSchema.methods.generateAuthToken = async function() {
   await user.save();
 
   return token;
+};
+
+userSchema.methods.toJSON = function() {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.tokens;
+  return userObject;
 };
 
 // Create new method for User model
@@ -80,6 +95,13 @@ userSchema.pre('save', async function(next) {
   if(user.isModified('password')) {
     user.password = await bcrypt.hash(user.password, 8);
   }
+  next();
+});
+
+// Delete user tasks when user is removed
+userSchema.pre('remove', async function(next) {
+  const user = this;
+  await Task.deleteMany({author: user._id});
   next();
 });
 
