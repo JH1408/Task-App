026ea418a676d5f71ca1581bda36de70e3183f84
Josy Hartig <josy_hartig@yahoo.de>
2019-07-27 11:57:39 +0800

    add pagination, filtering, sorting to task route

--- src/routers/task-router.js
+++ src/routers/task-router.js
@@ -18,15 +18,36 @@ router.post('/tasks', auth, async (req, res) => {
   }
 });
 
-router.get('/tasks', auth, async (req, res) => {
-  try {
-    // const tasks = await Task.find({ author: req.user_id});
-    await req.user.populate('tasks').execPopulate();
-    res.send(req.user.tasks);
-  } catch(err) {
-    res.status(500).send();
-  }
- });
+// /tasks?completed=true
+// /tasks?limit=10
+// /tasks?limit=10&skip=0
+// /tasks?sortBy=createdAt:desc
+ router.get('/tasks', auth, async (req, res) => {
+   const match = {};
+   const sort = {};
+   if(req.query.completed) {
+     match.completed = req.query.completed === 'true';
+   }
+   if(req.query.sortBy) {
+     const parts = req.query.sortBy.split(':');
+     sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+   }
+   try {
+     await req.user.populate({
+       path: 'tasks',
+       match,
+       options: {
+         limit: parseInt(req.query.limit),
+         skip: parseInt(req.query.skip),
+         sort
+       }
+     }).execPopulate();
+     res.send(req.user.tasks);
+   } catch(err) {
+     res.status(500).send();
+   }
+  });
+
 
 router.get('/tasks/:id', auth, async (req, res) => {
   const _id = req.params.id;
