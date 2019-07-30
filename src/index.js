016ccfd27141d4071ff2a0048b369340ccea9190
Josy Hartig <josy_hartig@yahoo.de>
2019-07-30 18:11:01 +0800

    update port for deployment

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ const userRouter = require('./routers/user-router');
 const taskRouter = require('./routers/task-router');
 
 const app = express();
-const port = process.env.PORT||3000;
+
 
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-app.listen(port, () => {
-  console.log('Server started on port ' + port);
+app.listen(process.env.PORT, () => {
+  console.log('Server started on port ' + process.env.PORT);
 });
